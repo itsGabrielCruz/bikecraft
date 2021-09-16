5d6eac49f8b12da374eae8081e7583ef0c85e86b
Gabriel Cruz <itsgabrielcruz@gmail.com>
2021-09-16 19:26:46 -0300

    Slide.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+if(window.SimpleSlide) {
+    new SimpleSlide({
+        slide: "quote",   // nome do atributo data-side="principal".
+        time: 5000   // tempo de trasição dos slides.
+    });
+    
+    new SimpleSlide({
+        slide: "portfolio",   // nome do atributo data-side="principal".
+        time: 5000,   // tempo de trasição dos slides.
+        nav: true,
+    });
+}
